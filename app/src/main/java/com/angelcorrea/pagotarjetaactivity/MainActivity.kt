1ba90f6d6f68c8ccab6b166a1b2d0dcd2346db29
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-26 13:22:20 -0600

    Adding action

--- app/src/main/java/com/angelcorrea/pagotarjetaactivity/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/pagotarjetaactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.angelcorrea.pagotarjetaactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val numeroTarjetaEditText = findViewById<EditText>(R.id.editTextText) 
+        val fechaExpiracionEditText = findViewById<EditText>(R.id.editTextDate)
+        val cvvEditText = findViewById<EditText>(R.id.editTextText2)
+        val pagarButton = findViewById<Button>(R.id.button)
+
+        pagarButton.setOnClickListener {
+            val numeroTarjeta = numeroTarjetaEditText.text.toString()
+            val fechaExpiracion = fechaExpiracionEditText.text.toString()
+            val cvv = cvvEditText.text.toString()
+
+            if (numeroTarjeta.isEmpty() || fechaExpiracion.isEmpty() || cvv.isEmpty()) {
+                Toast.makeText(this, "Por favor, llena todos los campos.", Toast.LENGTH_SHORT).show()
+            } else {
+                val mensaje = """
+                    Número de Tarjeta: $numeroTarjeta
+                    Fecha de Expiración: $fechaExpiracion
+                    CVV: $cvv
+                """.trimIndent()
+                Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
